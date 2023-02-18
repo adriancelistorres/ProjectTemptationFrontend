90a53e2e17b53b1328bcebb3af64328e655063f0
DyerMandujano <dyer0206@gmail.com>
2023-02-18 12:27:35 -0500

    Agregando validaciones al Color y colocando dropdown de estados

--- src/app/components/m-color/add-color/add-color.component.ts
+++ src/app/components/m-color/add-color/add-color.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -21,11 +22,12 @@ export class AddColorComponent {
     private toastr: ToastrService,
     private fb: FormBuilder,
     private router: Router,
+    private _errorService: ErrorService
 
   ) {
     this.formColor = this.fb.group({
       name_col: ['', Validators.required],
-      state: ['1', Validators.required],
+      // state: ['1', Validators.required],
     });
 
   }
@@ -33,11 +35,19 @@ export class AddColorComponent {
   addColor() {
     const color: IColor = {
       name_col: this.formColor.get('name_col')?.value,
-      state: this.formColor.get('state')?.value,
+      state: 1,
     };
-    this._colorService.addColor(color).subscribe(() => {
+    this._colorService.addColor(color).subscribe(
+      //Definimos un objeto, el cual tiene dos propiedades: next y error
+      {
+        //next es una propiedad, el cual tiene como valor a una funcion flecha anonima sin paramatros y dentro de esta funcion colocamos un toastr en caso se haya añadido un color satisfactoriamente.
+        next:()=>{
       this.toastr.success('El color se agrego correctamente');
-    });
+    },
+    //De lo contrario, en la propiedad 'error' se mandará un mensaje erroneo si no se agregó correctamente el color
+    error:(e:HttpErrorResponse)=>{
+      this._errorService.msjError(e);
+    }});
 
 
   }
