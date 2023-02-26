99af69602c89d24d64f00a2211969e4c04990953
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-02-25 19:08:57 -0500

    Corrigiendo Correccion

--- src/app/components/m-detailincome/edit-detailincome/edit-detailincome.component.ts
+++ src/app/components/m-detailincome/edit-detailincome/edit-detailincome.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IDetailIncome } from 'src/app/interfaces/IDetailIncome';
 
 @Component({
   selector: 'app-edit-detailincome',
   templateUrl: './edit-detailincome.component.html',
   styleUrls: ['./edit-detailincome.component.css']
 })
-export class EditDetailincomeComponent {
+export class EditDetailincomeComponent {  
+  // formDetailIncome: FormGroup;
+  // id: number,
+  // listDetailIncome: IDetailIncome[] = [];
 
+  constructor(){
+
+  }
 }
