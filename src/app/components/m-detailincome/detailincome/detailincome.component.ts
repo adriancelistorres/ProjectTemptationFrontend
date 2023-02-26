c9b098f068ec25a62f29ea5491390008161c7617
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-02-25 19:56:35 -0500

    Editar una compra

--- src/app/components/m-detailincome/detailincome/detailincome.component.ts
+++ src/app/components/m-detailincome/detailincome/detailincome.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { IDetailIncome } from 'src/app/interfaces/IDetailIncome';
 import { DetailincomeService } from 'src/app/services/detailincome.service';
 import { ErrorService } from 'src/app/utils/error/error.service';
+import { EditDetailincomeComponent } from '../edit-detailincome/edit-detailincome.component';
 
 @Component({
   selector: 'app-detailincome',
@@ -12,6 +13,13 @@ import { ErrorService } from 'src/app/utils/error/error.service';
 export class DetailincomeComponent {
   listDetailIncome: IDetailIncome[] = [];
   searchText: any;
+
+
+  @ViewChild(EditDetailincomeComponent) editview!: EditDetailincomeComponent;
+  edit(id: number) {
+    this.editview.getOneDetail(id);
+    console.log(id)
+  }
 
   constructor(
     private _detailincomeService: DetailincomeService,
